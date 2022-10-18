ad015d50bafeac1bf8407995e64ee18381eb3b08
mikkel <mikkel@spinnerlabs.no>
2022-10-18 22:41:39 +0200

    add more models and migrations. add country model.

--- laravel/database/migrations/2022_10_18_192059_create_texts_table.php
+++ laravel/database/migrations/2022_10_18_192059_create_texts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTranslatorsTable extends Migration
+class CreateTextsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTranslatorsTable extends Migration
      */
     public function up()
     {
-        Schema::create('translators', function (Blueprint $table) {
+        Schema::create('texts', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateTranslatorsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('translators');
+        Schema::dropIfExists('texts');
     }
 }
